53dd4bdd3cb63e80146631a1a8ba7c11facb358f
Ihor Fedorchenko <invisiblepanda04@gmail.com>
2023-01-27 10:08:03 +0200

    human repo

---
+++ src/main/java/Repositories/HumanRepository.java
@@ -0,0 +1,12 @@
+package Repositories;
+
+import Entities.Human;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HumanRepository extends JpaRepository<Human, Long> {
+
+    Human getHumanById(Long id);
+
+}
