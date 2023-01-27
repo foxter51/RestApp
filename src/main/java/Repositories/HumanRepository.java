d039850b6d03edf884b2f055e6293260b0a8a2d4
Ihor Fedorchenko <invisiblepanda04@gmail.com>
2023-01-27 10:35:41 +0200

    remove unnecessary method

--- src/main/java/Repositories/HumanRepository.java
+++ src/main/java/Repositories/HumanRepository.java
@@ -5,8 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HumanRepository extends JpaRepository<Human, Long> {
-
-    Human getHumanById(Long id);
-
-}
+public interface HumanRepository extends JpaRepository<Human, Long> {}
