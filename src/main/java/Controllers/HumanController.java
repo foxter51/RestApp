72c94d4ce6d08a4f7c1c78855b7bde991a98e92b
Ihor Fedorchenko <invisiblepanda04@gmail.com>
2023-01-29 16:39:24 +0200

    Controller improvement

--- src/main/java/Controllers/HumanController.java
+++ src/main/java/Controllers/HumanController.java
@@ -2,11 +2,16 @@ package Controllers;
 
 import Entities.Human;
 import Exceptions.HumanNotFoundException;
+import Repositories.HumanRepository;
 import Services.HumanService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class HumanController {
@@ -14,13 +19,18 @@ public class HumanController {
     private final HumanService humanService;
 
     @Autowired
-    public HumanController(HumanService humanService) {
-        this.humanService = humanService;
+    public HumanController(HumanRepository humanRepository) {
+        this.humanService = new HumanService(humanRepository);
     }
 
     @GetMapping("/humans/{id}")
-    Human getHumanInfo(@PathVariable Long id){
-        return humanService.findHumanById(id)
-                .orElseThrow(() -> new HumanNotFoundException(id));
+    ResponseEntity<Human> getHumanInfo(@PathVariable Long id){
+        Optional<Human> human = humanService.findHumanById(id);
+        if(human.isPresent()){
+            return new ResponseEntity<>(human.get(), HttpStatus.OK);
+        }
+        else {
+            throw new HumanNotFoundException(id);
+        }
     }
 }
