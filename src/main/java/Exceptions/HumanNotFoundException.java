067b114fc3d24d6550e5e2a8f3b43b05a299a2e6
Ihor Fedorchenko <invisiblepanda04@gmail.com>
2023-01-29 16:40:42 +0200

    exception fix

--- src/main/java/Exceptions/HumanNotFoundException.java
+++ src/main/java/Exceptions/HumanNotFoundException.java
@@ -1,5 +1,9 @@
 package Exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class HumanNotFoundException extends RuntimeException{
 
     public HumanNotFoundException(Long id){
